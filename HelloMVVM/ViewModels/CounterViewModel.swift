bcf8acddfc104d6af029f9d9bccf571f20793f06
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-16 15:21:35 +0200

    View Model

---
+++ HelloMVVM/ViewModels/CounterViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  CounterViewModel.swift
+//  HelloMVVM
+//
+//  Created by Pablo Aguirre on 16/07/24.
+//
+
+import SwiftUI
+
+class CounterViewModel: ObservableObject {
+    
+    // never expose the Model which contain business logic!
+    @Published private var counter = Counter()
+    
+    var value: Int {
+        counter.value
+    }
+    
+    func increment() {
+        counter.increment()
+    }
+    
+}
