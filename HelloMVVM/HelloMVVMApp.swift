a921177c86209d7a5f8fbd080952165838f266fc
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-16 14:57:12 +0200

    Initial Commit

---
+++ HelloMVVM/HelloMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMVVMApp.swift
+//  HelloMVVM
+//
+//  Created by Pablo Aguirre on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
